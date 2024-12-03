f125a4deef7fa8fdec8f2da9eae092b93777e3e5
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-12-04 00:10:00 +0300

    Add project files.

---
+++ AppointmentSystem.Server/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace AppointmentSystem.Server.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
