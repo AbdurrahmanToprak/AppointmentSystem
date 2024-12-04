d608ba7e0491203814eb8af48c955c2b4243c96d
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-12-04 17:08:13 +0300

    role controller eklendi.

---
+++ AppointmentSystem.Server/Controllers/RoleController.cs
@@ -0,0 +1,62 @@
+﻿using AppointmentSystem.Server.Data;
+using AppointmentSystem.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentSystem.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RoleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetRoles()
+        {
+            var roles = _context.Roles.ToList();
+            if (roles.Any())
+            {
+                return Ok(roles);
+            }
+            return Ok(new { message = "Veri Yok." });
+        }
+        [HttpPost]
+        public IActionResult CreateRole(Role role)
+        {
+            if (role == null)
+            {
+                return BadRequest(new {message = "Geçersiz veri."});
+            }
+            _context.Roles.Add(role);
+            _context.SaveChanges();
+            return Ok(new {message = "Rol Başarıyla Eklendi."});
+        }
+        [HttpPut("{id}")]
+        public IActionResult EditRole(int id, Role role)
+        {
+            var existRole = _context.Roles.FirstOrDefault(x=>x.RoleId == id);
+            if (existRole == null)
+            {
+                return NotFound(new { message = "Veri Bulunamadı." });
+            }
+            existRole.RoleName = role.RoleName;
+            _context.SaveChanges();
+            return Ok(new { message = "Rol Başarıyla Güncellendi.", existRole });
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRole(int id)
+        {
+            var existRole = _context.Roles.FirstOrDefault(x => x.RoleId == id);
+            if (existRole == null)
+            {
+                return NotFound(new { message = "Veri Bulunamadı." });
+            }
+            _context.Remove(existRole);
+            _context.SaveChanges();
+            return Ok(new { message = "Rol Başarıyla Silindi." });
+        }
+    }
+}
